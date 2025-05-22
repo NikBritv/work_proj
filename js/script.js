26067b70e1c6e150d974d66283a28645c30db530
Ivan <example@gmail.com>
2025-05-22 11:55:52 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// const  category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+// const user = "Ivan"
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+const isChecked = false,
+      isClosed = false;
+console.log(isChecked || isClosed);
